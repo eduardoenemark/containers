ddf8c9ec45674d90cbf5f9cf7d99ed8324985ae5
eduardo <eduardo@hefesto.localhost>
2024-09-10 11:07:40 -0300

    add: TOR Proxy Image

---
+++ tor-proxy/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER_FILE=Containerfile
+NAME=$(awk -v FS== '/org.opencontainers.image.ref.name/{print $2}' $CONTAINER_FILE)
+VERSION=$(awk -v FS== '/org.opencontainers.image.version/{print $2}' $CONTAINER_FILE)
+
+podman run -d --rm \
+  --name tor-proxy-container \
+  -p 61987:61987 \
+  -p 61988:61988 \
+  localhost/$NAME:$VERSION
+
+podman ps
