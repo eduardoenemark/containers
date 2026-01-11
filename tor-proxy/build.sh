3763dbed3a95be23dab504faa605a51aae4cfd19
eduardoenemark <eduardoenemark@gmail.com>
2026-01-11 18:54:55 -0500

    refactoring.

--- tor-proxy/build.sh
+++ tor-proxy/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-CONTAINER_FILE=Containerfile
-NAME=$(awk -v FS== '/org.opencontainers.image.ref.name/{print $2}' $CONTAINER_FILE)
-VERSION=$(awk -v FS== '/org.opencontainers.image.version/{print $2}' $CONTAINER_FILE)
+echo "Building tor-proxy container image..."
 
-podman build --arch amd64 --tag $NAME:$VERSION --file $CONTAINER_FILE
+CONTAINER_FILE=Containerfile
+
+read -r NAME VERSION < <(awk '
+  BEGIN { to_remove_regex = ".*=|[\"\\\\/]+| +$|^ +" }
+  /org.opencontainers.image.ref.name/{gsub(to_remove_regex,"", $0); name=$0}
+  /org.opencontainers.image.version/{gsub(to_remove_regex,"", $0); version=$0} 
+  END{print name, version}' $CONTAINER_FILE)
+
+podman build \
+  --arch amd64 \
+  --build-arg CREATED_DATETIME="$(date -u  +'%Y-%m-%dT%H:%M:%S%:z')" \
+  --tag $NAME:$VERSION \
+  --file $CONTAINER_FILE
+
+echo "Finished building $NAME:$VERSION"
--- tor-proxy/start.sh
+++ tor-proxy/start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-CONTAINER_FILE=Containerfile
-NAME=$(awk -v FS== '/org.opencontainers.image.ref.name/{print $2}' $CONTAINER_FILE)
-VERSION=$(awk -v FS== '/org.opencontainers.image.version/{print $2}' $CONTAINER_FILE)
+podman-compose -f compose.yaml up
 
-podman run -d --rm \
-  --name tor-proxy-container \
-  -p 61988:61988 \
-  localhost/$NAME:$VERSION
-
-podman ps
