9a3835fe720983516c9bbb6002d1fd7af9728a30
eduardoenemark <eduardoenemark@gmail.com>
2026-07-30 23:36:16 -0300

    convert subprojects to independent git subrepositories

--- tor/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONTAINER_FILE=Containerfile-bookworm
-NAME=$(awk -v FS== '/org.opencontainers.image.ref.name/{print $2}' $CONTAINER_FILE)
-VERSION=$(awk -v FS== '/org.opencontainers.image.version/{print $2}' $CONTAINER_FILE)
-
-podman build --arch amd64 --cpuset-cpus 0-2 --memory 512m --tag $NAME:$VERSION --file $CONTAINER_FILE
